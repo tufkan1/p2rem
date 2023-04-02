c3d5569db2b9063ab6bc3bf0c77d7505a6e93662
tufkan1 <30411352+tufkan1@users.noreply.github.com>
2023-04-02 05:08:39 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+
+const pxValue = process.argv[2];
+
+if (!pxValue) {
+    console.log('Usage: p2r <px-value>');
+    process.exit(1);
+}
+
+const remValue = parseFloat(pxValue) / 16;
+
+console.log(`${remValue}rem`);
